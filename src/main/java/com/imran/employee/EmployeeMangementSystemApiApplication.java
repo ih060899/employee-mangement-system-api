046315a1aec8b7b6dd496fbb7e7c894a34dec96c
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-19 23:21:49 +0600

    initial commit

---
+++ src/main/java/com/imran/employee/EmployeeMangementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.imran.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imran/employee/EmployeeMangementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imran.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
