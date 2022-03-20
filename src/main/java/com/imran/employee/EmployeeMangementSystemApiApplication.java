965cfac24df58cb700bd9dfeb540c2b8ef728604
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-20 11:35:38 +0600

    add controller

--- src/main/java/com/imran/employee/EmployeeMangementSystemApiApplication.java
+++ src/main/java/com/imran/employee/EmployeeMangementSystemApiApplication.java
@@ -8,6 +8,7 @@ public class EmployeeMangementSystemApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMangementSystemApiApplication.class, args);
+//		System.out.println("heloo");
 	}
 
 }
