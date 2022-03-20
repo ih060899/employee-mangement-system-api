daef6e97dfc47bdb64989e4b353749a1ba0d581a
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-20 11:51:48 +0600

    add repository

---
+++ src/main/java/com/imran/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.imran.employee.repository;
+
+import com.imran.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+}
